942ef77b35b9e5f89566d43bcb18a430996b834f
SV3105 <snehavaghela3105@gmail.com>
2025-07-08 20:04:19 +0530

    backend

---
+++ server/controllers/userController.js
@@ -0,0 +1,44 @@
+import { generateToken } from "../config/utils";
+import User from "../models/User";
+import bcrypt from "bcrypt"
+
+export const signup = async () => {
+    const {fullName, email, password, bio} = req.body;
+    try {
+        if(!fullName || !email || !password || !bio){
+           return res.json({success: false, message: "Missing Details"}) 
+        }
+        const user = await User.findOne({email});
+
+        if(user){
+               return res.json({success: false, message: "Account already exists"}) 
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+        
+        const newUser = await User.create({
+            fullName, email, password:hashedPassword, bio
+        });
+
+        const token = generateToken(newUser._id)
+
+        res.json({success: true, userData: newUser, token , message: "Account created successfully"})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+} 
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const userData = awaitUser.findOne({email})
+
+        const isPasswordCorrect = await bcrypt.compare(process, userData.password)
+
+        
+    } catch (error) {
+        
+    }
+}
